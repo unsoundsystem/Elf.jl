aba7f170fd6482cce094a028bddfc642717f4b05
sasuseso <sinai471530@gmail.com>
2020-06-24 01:39:38 +0900

    added some utility functions

--- sample/elfdump64.jl
+++ sample/elfdump64.jl
@@ -1,81 +1,52 @@
 #!/bin/julia
 using ELF
 
-function elfdump(head::Array{UInt8, 1})
-	ehdr = Elf64_Ehdr(head)
-
-	if !IS_ELF(ehdr)
-		error("This is not an ELF file")
-	end
-
-	if ehdr.e_ident[EI_CLASS] != ELFCLASS64
-		error("Unknown class.", Int(ehdr.e_ident[EI_CLASS]))
-	end
-
-	if ehdr.e_ident[EI_DATA] != ELFDATA2MSB
-		error("Unknown endian.", Int(ehdr.e_ident[EI_DATA]))
-	end
-
-	shstr = Elf64_Shdr(head, ehdr.e_shoff+ehdr.e_shentsize*ehdr.e_shstrndx)
-
-	#セクション名を一覧表示
-	println("Sections:")
-	for i in 0:ehdr.e_shnum-1
-		shdr = Elf64_Shdr(head, ehdr.e_shoff + ehdr.e_shentsize * i)
-		sname = unsafe_string(pointer(head, shstr.sh_offset + shdr.sh_name + 1))
-		if sname == ".strtab" global str = shdr end
-		println("\t[$i]\t$sname")
-	end
-	println()
-
-	#セグメント一覧
-	println("Segments:")
-	for i in 0:ehdr.e_phnum-1
-		phdr = Elf64_Phdr(head, ehdr.e_phoff + ehdr.e_phentsize * i)
-		println("\t[$i]\t")
-		for j in 0:ehdr.e_shnum-1
-			shdr = Elf64_Shdr(head, ehdr.e_shoff + ehdr.e_shentsize * j)
-			size = shdr.sh_type != SHT_NOBITS ? shdr.sh_size : 0
-			if shdr.sh_offset < phdr.p_offset continue end
-			if shdr.sh_offset + size  > phdr.p_offset + phdr.p_filesz continue end
-			println("\t", unsafe_string(pointer(head, shstr.sh_offset + shdr.sh_name + 1)))
-		end
-	end
-	println()
-
-	# #シンボル名一覧表示
-	println("Symbols:")
-	for i in 0:ehdr.e_shnum-1
-		shdr = Elf64_Shdr(head, ehdr.e_shoff + ehdr.e_shentsize * i);
-		if shdr.sh_type != SHT_SYMTAB continue end
-		global sym = shdr
-		for j in 0:div(sym.sh_size, sym.sh_entsize)-1
-			symp = Elf64_Sym(head, sym.sh_offset + sym.sh_entsize * j)
-			if symp.st_name == 0 continue end
-			println("\t[$j]\t$(ELF64_ST_TYPE(symp.st_info))\t$(symp.st_size)\t$(unsafe_string(pointer(head, str.sh_offset + symp.st_name + 1)))")
-		end
-	end
-
-	println("Relocations:")
-	for i in 0:ehdr.e_shnum-1
-		shdr = Elf64_Shdr(head, ehdr.e_shoff + ehdr.e_shentsize * i)
-		if shdr.sh_type != SHT_REL && shdr.sh_type != SHT_RELA continue end
-		rel = shdr
-		for j in 0:div(rel.sh_size, rel.sh_entsize)-1
-			relp = Elf64_Rel(head, rel.sh_offset + rel.sh_entsize * j)
-			symp = Elf64_Sym(head, sym.sh_offset + (sym.sh_entsize * ELF64_R_SYM(relp.r_info)))
-			if symp.st_name == 0 continue end
-			println("\t[$j]\t$(ELF64_R_SYM(relp.r_info))\t$(unsafe_string(pointer(head, str.sh_offset + symp.st_name + 1)))")
-		end
-	end
-end
-
-
 function main()
-	if isempty(ARGS)
-		error("no file specified")
-	end
-	elfdump(read(ARGS[1]))
+    isempty(ARGS) && error("No file specifed.")
+
+    bin = read(ARGS[1])
+
+    ehdr = Elf64_Ehdr(bin)
+
+    !iself(ehdr) && error("Not an ELF binary.")
+
+    !isclass(ehdr, ELFCLASS64) && error("Unknown class.")
+
+    !isendian(ehdr, ELFDATA2MSB) && error("Unknown endian.")
+
+    # Print Section Names
+    println("Sections:")
+    for (i, s) in enumerate(collect(sections(bin)))
+        println("\t[$i]:\t$(s.first)")
+    end
+    println()
+
+    # Print Segments Names
+    println("Segments:")
+    for (i, s) in enumerate(collect(segments(bin)))
+        println("\t[$i]:\t$(s.first)")
+    end
+
+    # Print Symbols Names
+    println("Symbols:")
+    syms, rels = symbols(bin)
+
+    for (i, s) in enumerate(collect(syms))
+        println("\t[$i]:\t$(s.first)")
+    end
+
+    println()
+
+    if isempty(rels)
+        println("No relocations.")
+    else
+        println("Relocations:")
+        for (i, s) in enumerate(collect(rels))
+            println("\t[$i]:\t$(s.first)")
+        end
+    end
 end
 
-main()
+if abspath(PROGRAM_FILE) == @__FILE__
+    main()
+end
--- src/ELF.jl
+++ src/ELF.jl
@@ -1,12 +1,37 @@
 module ELF
 using Base
+using StaticArrays
+using DataStructures
 
-export Elf64_Ehdr, Elf64_Shdr, Elf64_Phdr, Elf64_Rel, Elf64_Rela, Elf64_Sym, IS_ELF, print_section, EI_CLASS, ELFCLASS64, EI_DATA, ELFDATA2MSB, SHT_NOBITS, SHT_SYMTAB, ELF64_ST_TYPE, SHT_REL, SHT_RELA, ELF64_R_SYM, read_name
+export Elf64_Ehdr,
+    Elf64_Shdr,
+    Elf64_Phdr,
+    Elf64_Rel,
+    Elf64_Rela,
+    Elf64_Sym,
+    print_section,
+    EI_CLASS,
+    ELFCLASS64,
+    EI_DATA,
+    ELFDATA2MSB,
+    SHT_NOBITS,
+    SHT_SYMTAB,
+    ELF64_ST_TYPE,
+    SHT_REL,
+    SHT_RELA,
+    ELF64_R_SYM,
+    read_name,
+    iself,
+    isclass,
+    isendian,
+    sections,
+    segments,
+    symbols
 
 const EI_CLASS = 5
 const ELFCLASS64 = UInt8(2)
 const EI_DATA = 6
-const ELFDATA2MSB = 1
+const ELFDATA2MSB = UInt64(1)
 const EV_CURRENT = 1
 const ELFOSABI_GNU = 4
 const ELFOSABI_LINUX = ELFOSABI_GNU
@@ -33,163 +58,241 @@ const SHT_SYMTAB = 2
 const SHT_REL = 9
 const SHT_RELA = 4
 
+"""
+    Elf64_Ehdr
+
+A type represents 64bit ELF header.
+"""
 struct Elf64_Ehdr
-	e_ident::Vector{UInt8}
-	e_type::Elf64_Half
-	e_machine::Elf64_Half
-	e_version::Elf64_Word
-	e_entry::Elf64_Addr
-	e_phoff::Elf64_Off
-	e_shoff::Elf64_Off
-	e_flags::Elf64_Word
-	e_ehsize::Elf64_Half
-	e_phentsize::Elf64_Half
-	e_phnum::Elf64_Half
-	e_shentsize::Elf64_Half
-	e_shnum::Elf64_Half
-	e_shstrndx::Elf64_Half
-	
-	function Elf64_Ehdr(head::Vector{UInt8})
-		new(head[1:16],
-		    convert(UInt16, head[17:18]),
-		    convert(UInt16, head[19:20]),
-		    convert(UInt32, head[21:24]),
-		    convert(UInt64, head[25:32]),
-		    convert(UInt64, head[33:40]),
-		    convert(UInt64, head[41:48]),
-		    convert(UInt32, head[49:52]),
-		    convert(UInt16, head[53:54]),
-		    convert(UInt16, head[55:56]),
-		    convert(UInt16, head[57:58]),
-		    convert(UInt16, head[59:60]),
-		    convert(UInt16, head[61:62]),
-		    convert(UInt16, head[63:64]))
-	end
+    e_ident::SVector{16,UInt8}
+    e_type::Elf64_Half
+    e_machine::Elf64_Half
+    e_version::Elf64_Word
+    e_entry::Elf64_Addr
+    e_phoff::Elf64_Off
+    e_shoff::Elf64_Off
+    e_flags::Elf64_Word
+    e_ehsize::Elf64_Half
+    e_phentsize::Elf64_Half
+    e_phnum::Elf64_Half
+    e_shentsize::Elf64_Half
+    e_shnum::Elf64_Half
+    e_shstrndx::Elf64_Half
 end
 
+Elf64_Ehdr(bin::Vector{UInt8}) = pointer(bin) |> Ptr{Elf64_Ehdr} |> unsafe_load
+
+"""
+    iself(bin) -> Bool
+
+Verify ELF Header with Magic numbers
+
+# Examples
+```julia-repl
+julia> iself(read("/path/to/binary"))
+true
+```
+
+# Arguments
+- `bin::Vector{UInt8}` : binary file to check.
+
+"""
+iself(ehdr::Elf64_Ehdr) = ehdr.e_ident[begin:4] == [ELFMAG1, ELFMAG2, ELFMAG3, ELFMAG4]
+
+isclass(ehdr::Elf64_Ehdr, magic::UInt8) = ehdr.e_ident[EI_CLASS] == magic
+
+isendian(ehdr::Elf64_Ehdr, endian::UInt64) = ehdr.e_ident[EI_DATA] == endian
+
+"""
+    Elf64_Shdr
+
+A type represents 64bit ELF section header.
+"""
 struct Elf64_Shdr
-	sh_name::Elf64_Word
-	sh_type::Elf64_Word                       			
-	sh_flags::Elf64_Xword
-	sh_addr::Elf64_Addr
-	sh_offset::Elf64_Off
-	sh_size::Elf64_Xword
-	sh_link::Elf64_Word
-	sh_info::Elf64_Word
-	sh_addralign::Elf64_Xword
-	sh_entsize::Elf64_Xword
-
-	function Elf64_Shdr(head::Vector{UInt8}, off::UInt)
-		N = head[off+1:off+64]
-		new(convert(UInt32, N[1:4]),
-		    convert(UInt32, N[5:8]),
-		    convert(UInt64, N[9:16]),
-		    convert(UInt64, N[17:24]),
-		    convert(UInt64, N[25:32]),
-		    convert(UInt64, N[33:40]),
-		    convert(UInt32, N[41:44]),
-		    convert(UInt32, N[45:48]),
-		    convert(UInt64, N[49:56]),
-		    convert(UInt64, N[57:64]))
-	end
+    sh_name::Elf64_Word
+    sh_type::Elf64_Word
+    sh_flags::Elf64_Xword
+    sh_addr::Elf64_Addr
+    sh_offset::Elf64_Off
+    sh_size::Elf64_Xword
+    sh_link::Elf64_Word
+    sh_info::Elf64_Word
+    sh_addralign::Elf64_Xword
+    sh_entsize::Elf64_Xword
 end
 
+Elf64_Shdr(bin::Vector{UInt8}, off::UInt) =
+    pointer(bin, off + 1) |> Ptr{Elf64_Shdr} |> unsafe_load
+
+function sections(bin::Vector{UInt8})
+    sections_info::OrderedDict{String,Elf64_Shdr} = OrderedDict()
+    ehdr = Elf64_Ehdr(bin)
+    shstr = Elf64_Shdr(bin, ehdr.e_shoff + ehdr.e_shentsize * ehdr.e_shstrndx)
+    for i = 0:(ehdr.e_shnum - 1)
+        shdr = Elf64_Shdr(bin, ehdr.e_shoff + ehdr.e_shentsize * i)
+        sname = unsafe_string(pointer(bin, shstr.sh_offset + shdr.sh_name + 1))
+        push!(sections_info, sname => shdr)
+    end
+
+    sections_info
+end
+
+"""
+    Elf64_Phdr
+
+A type represents 64bit ELF program header.
+"""
 struct Elf64_Phdr
-	p_type::Elf64_Word
-	p_flags::Elf64_Word
-	p_offset::Elf64_Off
-	p_vaddr::Elf64_Addr
-	p_paddr::Elf64_Addr
-	p_filesz::Elf64_Xword
-	p_memsz::Elf64_Xword
-	p_align::Elf64_Xword
-
-	function Elf64_Phdr(head::Vector{UInt8}, off::UInt)
-		N = head[off+1:off+64]
-		new(convert(UInt32, N[1:4]),
-		    convert(UInt32, N[5:8]),
-		    convert(UInt64, N[9:16]),
-		    convert(UInt64, N[17:24]),
-		    convert(UInt64, N[25:32]),
-		    convert(UInt32, N[33:36]),
-		    convert(UInt32, N[37:40]),
-		    convert(UInt32, N[41:44]))
-	end
+    p_type::Elf64_Word
+    p_flags::Elf64_Word
+    p_offset::Elf64_Off
+    p_vaddr::Elf64_Addr
+    p_paddr::Elf64_Addr
+    p_filesz::Elf64_Xword
+    p_memsz::Elf64_Xword
+    p_align::Elf64_Xword
 end
 
+Elf64_Phdr(bin::Vector{UInt8}, off::UInt) =
+    pointer(bin, off + 1) |> Ptr{Elf64_Phdr} |> unsafe_load
+
+function segments(bin::Vector{UInt8})
+    ehdr = Elf64_Ehdr(bin)
+    shstr = Elf64_Shdr(bin, ehdr.e_shoff + ehdr.e_shentsize * ehdr.e_shstrndx)
+    segments_info::OrderedDict{String,Elf64_Phdr} = OrderedDict()
+
+    for i = 0:(ehdr.e_phnum - 1)
+        phdr = Elf64_Phdr(bin, ehdr.e_phoff + ehdr.e_phentsize * i)
+        for j = 0:(ehdr.e_shnum - 1)
+            shdr = Elf64_Shdr(bin, ehdr.e_shoff + ehdr.e_shentsize * j)
+            size = shdr.sh_type != SHT_NOBITS ? shdr.sh_size : 0
+            if shdr.sh_offset < phdr.p_offset
+                continue
+            end
+            if shdr.sh_offset + size > phdr.p_offset + phdr.p_filesz
+                continue
+            end
+            seg_name = unsafe_string(pointer(bin, shstr.sh_offset + shdr.sh_name + 1))
+            push!(segments_info, seg_name => phdr)
+        end
+    end
+
+    segments_info
+end
+
+"""
+    Elf64_Sym
+
+A type represents 64bit ELF Symbol Table entry.
+"""
 struct Elf64_Sym
-	st_name::Elf64_Word
-	st_info::UInt8
-	st_other::UInt8
-	st_shndx::Elf64_Section
-	st_value::Elf64_Addr
-	st_size::Elf64_Xword
-
-	function Elf64_Sym(head::Vector{UInt8}, off::UInt)
-		N = head[off+1:off+24]
-		new(convert(UInt32, N[1:4]),
-		    N[6],
-		    N[5],
-		    convert(UInt16, N[7:8]),
-		    convert(UInt64, N[9:16]),
-		    convert(UInt64, N[17:24]))
-	end
+    st_name::Elf64_Word
+    st_info::UInt8
+    st_other::UInt8
+    st_shndx::Elf64_Section
+    st_value::Elf64_Addr
+    st_size::Elf64_Xword
 end
 
+Elf64_Sym(bin::Vector{UInt8}, off::UInt) =
+    pointer(bin, off + 1) |> Ptr{Elf64_Sym} |> unsafe_load
+
+
+"""
+    symbols(bin::Vector{UInt8}) -> (OrderedDict{String,Elf64_Sym}, OrderedDict{String,Elf64_Rel})
+
+Returns ELF symbols in the file.
+
+# Examples
+```julia-repl
+julia> symbols(read("/path/to/binary"))
+(OrderedDict{String,Elf64_Sym}(...),OrderedDict{String,Elf64_Rel}())
+```
+
+# Arguments
+- `bin::Vector{UInt8}`: ELF binary.
+"""
+
+function symbols(bin::Vector{UInt8})
+    ehdr = Elf64_Ehdr(bin)
+    symbols_info::OrderedDict{String,Elf64_Sym} = OrderedDict()
+    str = sections(bin)[".strtab"]
+
+    for i = 0:(ehdr.e_shnum - 1)
+        shdr = Elf64_Shdr(bin, ehdr.e_shoff + ehdr.e_shentsize * i)
+        if shdr.sh_type != SHT_SYMTAB
+            continue
+        end
+        global sym = shdr
+        for j = 0:(div(sym.sh_size, sym.sh_entsize) - 1)
+            symp = Elf64_Sym(bin, sym.sh_offset + sym.sh_entsize * j)
+            if symp.st_name == 0
+                continue
+            end
+            push!(
+                symbols_info,
+                unsafe_string(pointer(bin, str.sh_offset + symp.st_name + 1)) => symp,
+            )
+        end
+    end
+
+    symbols_rel_info::OrderedDict{String,Elf64_Rel} = OrderedDict()
+
+    for i = 0:(ehdr.e_shnum - 1)
+        shdr = Elf64_Shdr(bin, ehdr.e_shoff + ehdr.e_shentsize * i)
+        if shdr.sh_type != SHT_REL && shdr.sh_type != SHT_RELA
+            continue
+        end
+        rel = shdr
+        for j = 0:(div(rel.sh_size, rel.sh_entsize) - 1)
+            relp = Elf64_Rel(bin, rel.sh_offset + rel.sh_entsize * j)
+            symp =
+                Elf64_Sym(bin, sym.sh_offset + (sym.sh_entsize * ELF64_R_SYM(relp.r_info)))
+            if symp.st_name == 0
+                continue
+            end
+            push!(
+                symbols_rel_info,
+                unsafe_string(pointer(bin, str.sh_offset + symp.st_name + 1)) => relp,
+            )
+        end
+    end
+
+    symbols_info, symbols_rel_info
+end
+
+
+"""
+    Elf64_Rel
+
+A type represents 64bit ELF relocatable symbol.
+"""
 struct Elf64_Rel
-	r_offset::Elf64_Addr
-	r_info::Elf64_Xword
-	
-	function Elf64_Rel(head::Vector{UInt8}, off::UInt)
-		N = head[off+1:off+16]
-		new(convert(UInt64, N[1:8]),
-		    convert(UInt64, N[9:16]))
-	end
+    r_offset::Elf64_Addr
+    r_info::Elf64_Xword
+
+    # function Elf64_Rel(head::Vector{UInt8}, off::UInt)
+        # N = head[(off + 1):(off + 16)]
+        # new(convert(UInt64, N[1:8]), convert(UInt64, N[9:16]))
+    # end
 end
 
+Elf64_Rel(bin::Vector{UInt8}, off::UInt) =
+    pointer(bin, off + 1) |> Ptr{Elf64_Rel} |> unsafe_load
 # struct Elf64_Rela
-	# r_offset::Elf64_Addr
-	# r_info::Elf64_Xword
-	# r_addend::Elf64_Sxword
-	# function Elf64_Rela(head::Vector{UInt8}, off::UInt)
-		# N = head[off+1:off:24]
-		# new(byte_array2uint64(N[1:8]),
-		    # byte_array2uint64(N[9:16]),
-		    # Int64(byte_array2uint64(N[17:24])))
-	# end
+# r_offset::Elf64_Addr
+# r_info::Elf64_Xword
+# r_addend::Elf64_Sxword
+# function Elf64_Rela(head::Vector{UInt8}, off::UInt)
+# N = head[off+1:off:24]
+# new(byte_array2uint64(N[1:8]),
+# byte_array2uint64(N[9:16]),
+# Int64(byte_array2uint64(N[17:24])))
 # end
-
-
-function IS_ELF(ehdr::Elf64_Ehdr)
-	return (ehdr.e_ident[EIMAG1] == ELFMAG1 &&
-		ehdr.e_ident[EIMAG2] == ELFMAG2 &&
-		ehdr.e_ident[EIMAG3] == ELFMAG3 &&
-		ehdr.e_ident[EIMAG4] == ELFMAG4)
-end
+# end
 
 ELF64_ST_TYPE(val) = val & 0xf
 
 ELF64_R_SYM(i) = i >>> 32
-
-function convert(T::Type{UInt32}, N::Vector{UInt8})
-	while length(N) % 4 != 0
-		prepend!(N, 0x00)
-	end
-	return reinterpret(UInt32, N)[1]
-end
-
-function convert(T::Type{UInt64}, N::Vector{UInt8})
-	while length(N) % 8 != 0
-		prepend!(N, 0x00)
-	end
-	return reinterpret(UInt64, N)[1]
-end
-
-function convert(T::Type{UInt16}, N::Vector{UInt8})
-	while length(N) % 2 != 0
-		prepend!(N, 0x00)
-	end
-	return reinterpret(UInt16, N)[1]
-end
 
 end # module
