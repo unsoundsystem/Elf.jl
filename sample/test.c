79cb73c61c03b0db6bfe4091f2eda160a67f8e00
sasuseso <sinai471530@gmail.com>
2020-06-29 18:42:07 +0900

    modified project file

--- sample/test.c
+++ sample/test.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 
-int main(int argc, char *argv[])
-{
-								printf("Hello, World");
-								return 0;
+int main(int argc, char *argv[]) {
+    printf("Hello, World");
+    return 0;
 }
 
